a3383531cbb912f34b8110b254b44eb5f2f12e3f
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-24 15:01:37 +0800

    add music bot channel commands functions

--- cogs/music_cog.py
+++ cogs/music_cog.py
@@ -13,3 +13,60 @@ from youtube_dl import YoutubeDL
 class music_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        self.is_playing = {}
+        self.is_paused = {}
+        self.musicQueue = {}
+        self.queueIndex = {}
+
+        self.YTDL_OPTIONS = {"format": "bestaudio", "nonplaylist": "True"}
+        self.FFMPEG_OPTIONS = {
+            "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+            "options": "-vn",
+        }
+
+        self.vc = {}
+
+    @commands.Cog.listener()
+    # run every time bot is rebooted
+    async def on_ready(self):
+        for guild in self.bot.guilds:
+            id = int(guild.id)
+            self.musicQueue[id] = []
+            self.queueIndex[id] = 0
+            self.vc[id] = None
+            self.is_paused[id] = self.is_playing[id] = False
+
+    # join voice channel command
+    async def join_VC(self, ctx, channel):
+        id = int(ctx.guild.id)
+        if self.vc[id] == None or not self.vc[id].is_connected():
+            self.vc[id] = await channel.connect()
+
+            if self.vc[id] == None:
+                await ctx.send("Could not connect to the voice channel.")
+                return
+        else:
+            await self.vc[id].move_to(channel)
+
+    def search_YT(self, search):
+        queryString = parse.urlencode({"search_query": search})
+        htmContent = request.urlopen("https://www.youtube.com/results?" + queryString)
+        searchResults = re.findall("/watch\?v=(.{11})", htmContent.read().decode())
+        return searchResults[0:10]
+
+    def extract_YT(self, url):
+        with YoutubeDL(self.YTDL_OPTIONS) as ydl:
+            try:
+                info = ydl.extract_info(url, download=False)
+            except:
+                return False
+
+        return {
+            "link": "https://www.youtube.com/watch?v=" + url,
+            "thumbnail": "https://i.ytimg.com/vi/"
+            + url
+            + "/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD5uL4xKN-IUfez6KIW_j5y70mlig",
+            "source": info["formats"][0]["url"],
+            "title": info["title"],
+        }
